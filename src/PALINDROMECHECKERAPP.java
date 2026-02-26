fb0fb9803942556bfe676e2ae793b2073a56b2b3
sushmandavasri27 <sushmandavasri27@gmail.com>
2026-02-26 15:46:47 +0530

    Updated Palindrome Checker code

--- src/PALINDROMECHECKERAPP.java
+++ src/PALINDROMECHECKERAPP.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
 public class PALINDROMECHECKERAPP {
-}
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter String for Deque check: ");
+            String input = scanner.nextLine();
+
+            Deque<Character> deque = new ArrayDeque<>();
+            for(char c : input.toCharArray()){
+                deque.addLast(c);
+            }
+            boolean isPalindrome = true;
+            while (deque.size() >1){
+                if (!deque.removeFirst().equals(deque.removeLast())){
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            System.out.println("Input:" + input);
+            System.out.println("Is Palindrome? : " + isPalindrome);
+            scanner.close();
+        }
+    }
